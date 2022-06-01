94517b49ea9ba4dc58c49b57af415d28313c0055
JerryProChac <93705083+JerryProChac@users.noreply.github.com>
2022-06-01 14:09:51 +0200

    1

--- mian.py
+++ mian.py
@@ -1,7 +1,17 @@
 import pandas as pd
+import seaborn as sns
 
-df = pd.read_excel('novy_bez.xlsx')
+df = pd.read_excel('s2220106.xlsx')
+df.to_csv('mod.cvs', index=False)
 
-df.to_csv('mod.cvs', index=True)
+print(df.columns[0])
+print(df[df.columns[0]] == 'H')
 
-print(df.iloc[0, 0])
+a = df[df.columns[0]] == 'H'
+
+print(df.index[0])
+
+
+
+
+
